3c99519c5a82518df598faabc859108239f75414
Andrew Wandiga <hello@wandiga.com>
2026-01-05 18:13:13 +0300

    Downgrade Tailwind to v 3^

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",          // all HTML files in root
+    "./src/**/*.{html,js,jsx}", // all files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
